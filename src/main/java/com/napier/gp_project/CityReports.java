79a650453276839e7cf3f2e9631b71edb40acb1b
KaungHtetSan29 <40780473@live.napier.ac.uk>
2025-10-13 13:44:10 +0630

    Sprint one City Reports push

--- src/main/java/com/napier/gp_project/CityReports.java
+++ src/main/java/com/napier/gp_project/CityReports.java
@@ -15,10 +15,7 @@ public class CityReports {
      */
     public void getCitiesInWorld() {
         try {
-            String sql = "SELECT city.Name AS CityName, " +
-                    "country.Name AS CountryName, " +
-                    "city.District, " +
-                    "city.Population " +
+            String sql = "SELECT city.Name AS CityName, country.Name AS CountryName, city.District, city.Population " +
                     "FROM city " +
                     "JOIN country ON city.CountryCode = country.Code " +
                     "ORDER BY city.Population DESC";
@@ -50,5 +47,42 @@ public class CityReports {
         }
     }
 
+    /**
+     * Top N populated cities in the world.
+     */
+    public void getTopNPopulatedCitiesInWorld(int n) {
+        try {
+            String sql = "SELECT city.Name AS CityName, country.Name AS CountryName, city.District, city.Population " +
+                    "FROM city " +
+                    "JOIN country ON city.CountryCode = country.Code " +
+                    "ORDER BY city.Population DESC LIMIT ?";
+
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, n);
+            ResultSet rset = pstmt.executeQuery();
+
+            ArrayList<City> cities = new ArrayList<>();
+
+            while (rset.next()) {
+                City c = new City();
+                c.setName(rset.getString("CityName"));
+                c.setCountryCode(rset.getString("CountryName"));
+                c.setDistrict(rset.getString("District"));
+                c.setPopulation(rset.getInt("Population"));
+                cities.add(c);
+            }
+
+            System.out.println("\nTop " + n + " Populated Cities in the World");
+            System.out.printf("%-35s %-25s %-20s %15s%n", "City Name", "Country", "District", "Population");
+
+            for (City c : cities) {
+                System.out.printf("%-35s %-25s %-20s %,15d%n",
+                        c.getName(), c.getCountryCode(), c.getDistrict(), c.getPopulation());
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error generating top N world city report: " + e.getMessage());
+        }
+    }
 
 }
