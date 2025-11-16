19096897470be2b6aaa1973b4d19062f60b7a088
40794504-HtooHtetAung <40794504@live.napier.ac.uk>
2025-11-16 14:38:15 +0700

    integration test example run + codecov plugin

---
+++ src/test/java/com/napier/gp_project/AppIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.napier.gp_project;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest {
+    static App app;
+    Capital_city_reports capital_city_reports;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    @Test
+    void getAllCapitalCitiesInWorld()
+    {
+        capital_city_reports.con = app.getConnection();
+        capital_city_reports = new Capital_city_reports();
+        capital_city_reports.getAllCapitalCitiesInWorld();
+    }
+}
